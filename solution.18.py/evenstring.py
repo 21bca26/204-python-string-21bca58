0524f2c222faecaf2f75abca6a2980803c004716
21bca26 <96609077+21bca26@users.noreply.github.com>
2022-03-30 10:31:56 +0530

    Update evenstring.py
    you have too count the letters of i and then count it and modul it

--- solution.18.py/evenstring.py
+++ solution.18.py/evenstring.py
@@ -11,8 +11,8 @@ def strlen(l):
     count=0
     for i in l:
         for j in i:
-            if(l%2==0):
-                count+=l
+           // if(l%2==0):
+              //  count+=l
     return(count)            
     
         
